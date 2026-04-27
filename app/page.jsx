d09f5048bcf84aced705a7158b1ab38bf2244d12
guruduth6be29-glitch <guruduth6.be29@gmail.com>
2026-04-27 17:26:29 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,14 @@ export default function Home() {
 return (
 
 Chowdeshwari Temple
-Welcome to Chowdeshwari Temple
+Welcome to our temple website
 Address: 1st Cross, 7th Main, Shrirampuram, Bangalore - 560021
 Contact: 9845128868
-Morning: 6:00 AM – 12:00 PM
-Evening: 6:00 PM – 9:00 PM
+Morning: 6:00 AM to 12:00 PM
+Evening: 6:00 PM to 9:00 PM
+Abhishekam: ₹300
+Annadanam Available
+Dasara Alankara Booking Available in October
 
 );
 }
